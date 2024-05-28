388ea826ec6a52ace8276fa3c7978d05f3d0d436
陶奇林 <1624362469@qq.com>
2024-05-28 15:59:47 +0800

    init

---
+++ assets/502-DuSss1Bw.js
@@ -0,0 +1,1 @@
+import{E as e}from"./error-page-Cy1Sobft.js";import{d as o,c as r,o as a}from"./index-CXFD5fok.js";const _=o({__name:"502",setup(c){return(t,s)=>(a(),r(e,{code:502,desc:"马上回来"}))}});export{_ as default};
