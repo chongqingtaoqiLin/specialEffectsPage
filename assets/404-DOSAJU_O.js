dad48d73a3dc3a12c98581cb54bbda2747399cce
陶奇林 <1624362469@qq.com>
2025-01-04 10:35:53 +0800

    拖拽表单

---
+++ assets/404-DOSAJU_O.js
@@ -0,0 +1,1 @@
+import{E as e}from"./error-page-Cxk6SaJ4.js";import{d as o,c as r,o as a}from"./index-B-K_pQE_.js";const _=o({__name:"404",setup(c){return(t,s)=>(a(),r(e,{code:404,desc:"找不到您要查找的页面"}))}});export{_ as default};
--- assets/500-D5r99XdP.js
+++
@@ -1,1 +0,0 @@
-import{E as e}from"./error-page-DiZZCcHU.js";import{d as o,c as r,o as a}from"./index-DUkGJVAd.js";const _=o({__name:"500",setup(c){return(t,s)=>(a(),r(e,{code:500,desc:"糟糕，出了点问题"}))}});export{_ as default};
